6cc85deae328ba2703a42283d837eeddb984bb26
Antoine B <antoine.bourricaud@etudiant.univ-lr.fr>
2025-10-05 21:10:56 +0200

    Ajout de l'archer

---
+++ Assets/Archer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class Archer : MoveableObjects
+{
+    [SerializeField] GameObject prefabArrow;
+    [SerializeField] Transform spawnArrow;
+    [SerializeField] float arrowForce = 10f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnLoop());
+    }
+
+    IEnumerator SpawnLoop()
+    {
+        while (true && transform.position.x >= DrakeController.instance.transform.position.x+4f)
+        {
+            yield return new WaitForSeconds(2.5f);
+            ShootArrow();
+        }
+    }
+
+    private void ShootArrow()
+    {
+        GameObject arrow = Instantiate(prefabArrow, spawnArrow.position, Quaternion.identity);
+
+        Vector2 direction = (DrakeController.instance.transform.position - spawnArrow.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        arrow.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * arrowForce, ForceMode2D.Impulse);
+
+        Destroy(arrow, 4f);
+    }
+}
--- Assets/Scripts/Gameplay/MoveableObjects.cs
+++ Assets/Scripts/Gameplay/MoveableObjects.cs
@@ -4,6 +4,11 @@ public class MoveableObjects : MonoBehaviour
 {
     public float speed = 1.5f;
 
+    void Start()
+    {
+        Destroy(gameObject, 6f);
+    }
+
     protected void FixedUpdate()
     {
         transform.position -= new Vector3(speed, 0, 0) * Time.fixedDeltaTime;
